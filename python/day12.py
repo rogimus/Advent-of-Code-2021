ee2286e1fe734fd349d744e2c1de7aa5479a02d5
Roger Murray <roger.h.murray@gmail.com>
2022-02-23 23:37:40 -0500

    adding day12

---
+++ python/day12.py
@@ -0,0 +1,29 @@
+import sys
+from queue import Queue
+from collections import defaultdict
+
+inp = sys.stdin.read().split("\n")[:-1]
+data = [x.split("-") for x in inp]
+adj = defaultdict(list)
+for i in data:
+    adj[i[0]].append(i[1])
+    adj[i[1]].append(i[0])
+
+def dfs(v, n, finish):
+    if n == 'end':
+        return 1
+
+    t=0
+    for i in adj[n]:
+        if i != 'start' and i.islower() and i in v and not finish:
+            t += dfs(v.union({i}),i, True)
+        if i.islower() and i not in v:
+            t += dfs(v.union({i}), i, finish)
+        elif i.isupper():
+            t += dfs(v,i, finish)
+            
+    return t
+
+visited = {'start'}
+print(dfs(visited,'start', True)) # part 1
+print(dfs(visited, 'start', False)) # part 2
